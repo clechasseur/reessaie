3e303275fe3a90b3e2678020ac0d203c4294c5c0
Charles Lechasseur <shiftingbeard@outlook.com>
2025-10-05 00:22:45 -0400

    chore: initial project setup

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! # `reessaie`
+//!
+//! _TODO_
+
+#![deny(missing_docs)]
+#![deny(rustdoc::missing_crate_level_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg_hide))]
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
